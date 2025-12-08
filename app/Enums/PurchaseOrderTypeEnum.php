8e07565a1cdd0e41d8d486768c1db9ca3a65c462
mostafa.mossad <mostafamosaad202@gmail.com>
2025-12-08 16:23:05 +0200

    change the potype enum

--- app/Enums/PurchaseOrderTypeEnum.php
+++ app/Enums/PurchaseOrderTypeEnum.php
@@ -6,8 +6,8 @@ enum PurchaseOrderTypeEnum: int
 {
     case LOCAL_PO_SUPPLIER_0 = 0;
     case LOCAL_PO_SUPPLIER_1 = 1;
-    case TRANSFER_IN = 2;
-    case TRANSFER_OUT = 3;
+    case TransferIN = 2;
+    case TransferOut = 3;
     case TRANSFER_IN_HQ = 4;
     case TRANSFER_OUT_HQ = 5;
 }
